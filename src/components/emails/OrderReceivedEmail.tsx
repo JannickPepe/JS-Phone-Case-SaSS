b1ae58e5c0b37d8f25d054fd5732f488901dcd8e
jannick <jannickpedersen@live.dk>
2024-06-20 14:01:10 +0200

    implemented thank you page and admin order page

---
+++ src/components/emails/OrderReceivedEmail.tsx
@@ -0,0 +1,11 @@
+import { ShippingAddress } from '@prisma/client';
+import React from 'react';
+import {Html} from "@react-email/components";
+
+
+const OrderReceivedEmail = ({shippingAddress,}: {shippingAddress: ShippingAddress}) => {
+    <Html></Html>
+};
+
+
+export default OrderReceivedEmail;
